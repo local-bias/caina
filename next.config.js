e99ae46bbcc3535e2605da433f1d4e5e81a1b63c
Ribbit <echo07200503@gmail.com>
2022-02-19 22:58:49 +0900

    🌐 ロケール設定を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  locales: ['ja-JP', 'en-US'],
+  defaultLocale: 'ja-JP',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
